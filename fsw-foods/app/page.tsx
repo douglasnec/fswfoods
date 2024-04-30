ca213a167fe3e6b3cbf920c0beefddc3177d9c0f
Douglas Nery <douglasnec@hotmail.com>
2024-04-30 16:52:38 -0300

    feat: add initial header component

--- fsw-foods/app/page.tsx
+++ fsw-foods/app/page.tsx
@@ -1,7 +1,6 @@
+import Header from './_components/header'
 export default function Home() {
   return (
-    <div className="bg-red-500 p-5">
-      <h1>Hello tailwind!</h1>
-    </div>
+    <Header />
   );
 }

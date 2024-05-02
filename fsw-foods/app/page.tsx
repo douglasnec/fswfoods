f53360b6e03b2c87e2241a6a55aca1955af629ff
Douglas Nery <douglasnec@hotmail.com>
2024-05-02 16:03:48 -0300

    feat: add second promo banner to home page

--- fsw-foods/app/page.tsx
+++ fsw-foods/app/page.tsx
@@ -6,6 +6,7 @@ import ProductList from './_components/product-list';
 import { Button } from './_components/ui/button';
 import { ChevronRightIcon } from 'lucide-react';
 import { db } from './_lib/prisma';
+import PromoBanner from './_components/promo-banner';
 
 const Home = async () => {
 
@@ -36,16 +37,7 @@ const Home = async () => {
     </div>
 
     <div className="px-5 pt-6">
-
-      <Image
-        src="/promo-banner-01.png"
-        alt='Até 30% de desconto em pizza!'
-        height={0}
-        width={0}
-        className='h-auto w-full object-contain'
-        sizes="100vw"
-        quality={100}
-      />
+      <PromoBanner src="/promo-banner-01.png" alt="Até 30% de desconto em Pizzas"/>
     </div>
 
     <div className='pt-6 space-y-0'>
@@ -57,6 +49,10 @@ const Home = async () => {
         </Button>
       </div>
       <ProductList products={products} />
+    </div>
+
+    <div className="px-5 pt-6">
+      <PromoBanner src="/promo-banner-02.png" alt="A partir de R$17,90 em lanches"/>
     </div>
   </>
   );

43c1aa2d99f781444e1ac443fa51793213d0e571
Douglas Nery <douglasnec@hotmail.com>
2024-05-02 10:41:09 -0300

    feat: add promo banner to  homepage

--- fsw-foods/app/_components/category-item.tsx
+++ fsw-foods/app/_components/category-item.tsx
@@ -7,7 +7,7 @@ interface CategoryItemProps {
 
 const CategoryItem = ({ category }: CategoryItemProps) => {
     return (
-        <div className="flex items-center gap-3 bg-white px-4 py-3 shadow-md rounded-full">
+        <div className="flex items-center gap-3 bg-white px-4 py-3 shadow-md rounded-full min-w-[160px]">
             <Image src={category.imageUrl} alt={category.name} height={30} width={30} />
             <span className='font-semibold text-sm'>{category.name}</span>
         </div>
--- fsw-foods/app/page.tsx
+++ fsw-foods/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from './_components/header'
 import Search from './_components/search'
 import CategoryList from './_components/category-list'
+import Image from 'next/image'
 
 export default function Home() {
   return (<>
@@ -11,6 +12,19 @@ export default function Home() {
 
     <div className="px-5 pt-6">
       <CategoryList />
+    </div>
+
+    <div className="px-5 pt-6">
+
+      <Image
+        src="/promo-banner-01.png"
+        alt='Até 30% de desconto em pizza!'
+        height={0}
+        width={0}
+        className='h-auto w-full object-contain'
+        sizes="100vw"
+        quality={100}
+      />
     </div>
   </>
   );
